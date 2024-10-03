555d3d978d0b7ff6e42b6ea0bdd452014c273367
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2024-10-03 14:22:56 +0000

    fix repo

---
+++ exercise1.c
@@ -0,0 +1,15 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        int thread_num = omp_get_thread_num();
+        if (thread_num % 2 == 0) {
+            printf("Hola desde el hilo par %d\n", thread_num);
+        } else {
+            printf("Hola desde el hilo impar %d\n", thread_num);
+        }
+    }
+    return 0;
+}
---
+++ exercise2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define SIZE 100
+
+int main() {
+    int NUMHILO = 4;
+    int array[SIZE];
+    int max_val, min_val;
+    double avg_val;
+    int sum = 0;
+
+    // Initialize the array with random numbers
+    #pragma omp parallel num_threads(NUMHILO)
+    {
+        int thread_num = omp_get_thread_num();
+        int inicio =  25 * thread_num;
+        int fin =  (25 * (thread_num + 1)) - 1;
+        printf("Hola desde el hilo %d me encargo de los valores de %d hasta %d\n", thread_num , inicio , fin);
+        for (int i = inicio; i < fin; i++) {
+            array[i] = rand() % 1000; // Random numbers between 0 and 999
+        }
+    }
+
+    // Find the max, min, and average values
+    max_val = array[0];
+    min_val = array[0];
+
+    #pragma omp parallel for reduction(+:sum) reduction(max:max_val) reduction(min:min_val)
+    for (int i = 0; i < SIZE; i++) {
+        if (array[i] > max_val) max_val = array[i];
+        if (array[i] < min_val) min_val = array[i];
+        sum += array[i];
+    }
+
+    avg_val = sum / (double)SIZE;
+
+    printf("Max value: %d\n", max_val);
+    printf("Min value: %d\n", min_val);
+    printf("Average value: %.2f\n", avg_val);
+
+    return 0;
+}
